434029ee34661d76bb56b65f6a320b5ac60e2016
cc8carat <chloe26.ni@gmail.com>
2022-01-10 11:01:03 +0100

    add ResponsiveContainer in chart

--- src/components/BiorhythmsCard.js
+++ src/components/BiorhythmsCard.js
@@ -15,8 +15,8 @@ const BiorhythmsCard = ({ targetDate, formatDate, dateOfBirth }) => {
       <IonCardContent>
         <BiorthythmChart dateOfBirth={dateOfBirth} targetDate={targetDate} />
         <p className='physical'>Physical: {physical.toFixed(4)}</p>
-        <p>Emotional: {emotional.toFixed(4)}</p>
-        <p>Intellectual: {intellectual.toFixed(4)}</p>
+        <p className='emotional'>Emotional: {emotional.toFixed(4)}</p>
+        <p className='intellectual'>Intellectual: {intellectual.toFixed(4)}</p>
       </IonCardContent>
     </IonCard>
   );
--- src/components/BiorthythmChart.js
+++ src/components/BiorthythmChart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Line, LineChart, XAxis, ReferenceLine, CartesianGrid } from 'recharts';
+import { Line, LineChart, XAxis, ReferenceLine, CartesianGrid, ResponsiveContainer } from 'recharts';
 import dayjs from 'dayjs';
 import { calculateBiorhythmsSeries } from '../calculations';
 const BiorthythmChart = ({ dateOfBirth, targetDate }) => {
@@ -8,14 +8,16 @@ const BiorthythmChart = ({ dateOfBirth, targetDate }) => {
   const data = calculateBiorhythmsSeries(dateOfBirth, startDate, 31).map((element) => ({ ...element, date: formatDate(element.date) }));
 
   return (
-    <LineChart width={260} height={200} data={data}>
-      <XAxis dataKey='date' ticks={[data[3].date, data[15].date, data[27].date]} />
-      <ReferenceLine x={data[15].date} />
-      <CartesianGrid vertical={false} strokeDasharray='3 3' />
-      <Line dot={false} type='natural' dataKey='physical' stroke='green' />
-      <Line dot={false} type='natural' dataKey='emotional' stroke='blue' />
-      <Line dot={false} type='natural' dataKey='intellectual' stroke='red' />
-    </LineChart>
+    <ResponsiveContainer width='100%' height={200}>
+      <LineChart width={260} height={200} data={data}>
+        <XAxis dataKey='date' ticks={[data[3].date, data[15].date, data[27].date]} />
+        <ReferenceLine x={data[15].date} />
+        <CartesianGrid vertical={false} strokeDasharray='3 3' />
+        <Line dot={false} type='natural' dataKey='physical' stroke='green' />
+        <Line dot={false} type='natural' dataKey='emotional' stroke='blue' />
+        <Line dot={false} type='natural' dataKey='intellectual' stroke='red' />
+      </LineChart>
+    </ResponsiveContainer>
   );
 };
 
